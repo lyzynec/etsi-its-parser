ddf6a0c2be7818287ab1ba533754395bc1bee8d8
Matěj Kříž <lyzynec@protonmail.com>
2024-07-12 13:35:28 +0200

    Minor simplification of the json structure.

--- src/build_json.cpp
+++ src/build_json.cpp
@@ -2,17 +2,10 @@
 
 Document buildJSON(DENM_t message, json_context_t context) {
 
-    ITS_Container_ItsPduHeader_t &header = message.header;
-
     Document document(kObjectType);
     Document::AllocatorType &allocator = document.GetAllocator();
 
     document.AddMember("timestamp", context.time_reception, allocator)
-            .AddMember("rssi", context.rssi, allocator)
-            .AddMember("stationID", Value(static_cast<int64_t>(header.stationID)), allocator)
-            .AddMember("receiverID", context.receiver_id, allocator)
-            .AddMember("receiverType", context.receiver_type, allocator)
-            .AddMember("packet_size", context.packet_size, allocator)
             .AddMember("fields", to_json(message, allocator), allocator);
 
     return document;
@@ -20,36 +13,21 @@ Document buildJSON(DENM_t message, json_context_t context) {
 
 Document buildJSON(CAM_t message, json_context_t context) {
 
-    ITS_Container_ItsPduHeader_t &header = message.header;
-
     Document document(kObjectType);
     Document::AllocatorType &fullAllocator = document.GetAllocator();
 
-    //.AddMember("newInfo", new_info, fullAllocator)
     document.AddMember("timestamp", context.time_reception, fullAllocator)
-            .AddMember("rssi", context.rssi, fullAllocator)
-            .AddMember("stationID", Value(static_cast<int64_t>(header.stationID)), fullAllocator)
-            .AddMember("receiverID", context.receiver_id, fullAllocator)
-            .AddMember("receiverType", context.receiver_type, fullAllocator)
-            .AddMember("packet_size", context.packet_size, fullAllocator)
-            .AddMember("fields", to_json(message, fullAllocator), fullAllocator);
+        .AddMember("fields", to_json(message, fullAllocator), fullAllocator);
 
     return document;
 }
 
 Document buildJSON(SPATEM_t message, json_context_t context) {
 
-    ITS_Container_ItsPduHeader_t &header = message.header;
-
     Document document(kObjectType);
     Document::AllocatorType &allocator = document.GetAllocator();
 
     document.AddMember("timestamp", context.time_reception, allocator)
-            .AddMember("rssi", context.rssi, allocator)
-            .AddMember("stationID", Value(static_cast<int64_t>(header.stationID)), allocator)
-            .AddMember("receiverID", context.receiver_id, allocator)
-            .AddMember("receiverType", context.receiver_type, allocator)
-            .AddMember("packet_size", context.packet_size, allocator)
             .AddMember("fields", to_json(message, allocator), allocator);
 
     return document;
@@ -57,18 +35,10 @@ Document buildJSON(SPATEM_t message, json_context_t context) {
 
 Document buildJSON(MAPEM_t message, json_context_t context) {
 
-    ITS_Container_ItsPduHeader_t &header = message.header;
-
     Document document(kObjectType);
     Document::AllocatorType &allocator = document.GetAllocator();
 
-
     document.AddMember("timestamp", context.time_reception, allocator)
-            .AddMember("rssi", context.rssi, allocator)
-            .AddMember("stationID", Value(static_cast<int64_t>(header.stationID)), allocator)
-            .AddMember("receiverID", context.receiver_id, allocator)
-            .AddMember("receiverType", context.receiver_type, allocator)
-            .AddMember("packet_size", context.packet_size, allocator)
             .AddMember("fields", to_json(message, allocator), allocator);
 
     return document;
